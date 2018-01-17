c8d1d2664f07f6279f1725fa1a6ae7a7f79752ac
Dariusz Pawlukiewicz <foreverframenet@gmail.com>
2018-01-17 10:02:56 +0100

    Added OverridingAsyncMethodBuilder project

---
+++ OverridingAsyncMethodBuilder/AsyncTaskMethodBuilder.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+
+namespace System.Runtime.CompilerServices
+{
+    public struct AsyncTaskMethodBuilder
+    {
+        public static AsyncTaskMethodBuilder Create()
+        {
+            return new AsyncTaskMethodBuilder();
+        }
+
+        public void SetException(Exception e) { }
+        public void SetResult() { }
+        public Task Task { get { return null; } }
+
+        public void AwaitOnCompleted<TAwaiter, TStateMachine>(ref TAwaiter awaiter, ref TStateMachine stateMachine)
+              where TAwaiter : INotifyCompletion
+              where TStateMachine : IAsyncStateMachine
+        {
+            Console.WriteLine("I'm hacked!");
+        }
+
+        public void AwaitUnsafeOnCompleted<TAwaiter, TStateMachine>(ref TAwaiter awaiter, ref TStateMachine stateMachine)
+              where TAwaiter : ICriticalNotifyCompletion
+              where TStateMachine : IAsyncStateMachine
+        {
+            Console.WriteLine("I'm hacked!");
+        }
+
+        public void Start<TStateMachine>(ref TStateMachine stateMachine) where TStateMachine : IAsyncStateMachine
+        {
+            stateMachine.MoveNext();
+        }
+
+        public void SetStateMachine(IAsyncStateMachine stateMachine)
+        {
+        }
+    }
+}
---
+++ OverridingAsyncMethodBuilder/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace OverridingAsyncMethodBuilder
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await Task.Delay(1000);
+            Console.ReadLine();
+        }
+    }
+}
